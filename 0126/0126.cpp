12a8d416111a75c4f2b197f88b54071abd47c380
lim024811 <lim024811@naver.com>
2026-02-13 16:26:32 +0900

    입출력 함수

---
+++ 0126/0126.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main()
+{
+	int a;
+	printf("한 정수를 입력하세요: ");
+	scanf("%d", &a);
+	printf("입력한 값은 %d입니다\n", a);
+
+
+
+	/*
+	//printf("HelloC++"); //주석문(설명문)
+	//printf("HelloC++\n"); // \n: 엔터
+	*/	/*여러 줄 주석문*/
+
+	// puts("안녕하세요!");
+	
+}
