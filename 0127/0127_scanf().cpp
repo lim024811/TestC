4fab4029c45ae9860934a383bde5e6be61cba9c8
lim024811 <lim024811@naver.com>
2026-02-13 16:27:20 +0900

    scanf 심화

---
+++ 0127/0127_scanf().cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+	/*
+    int a, b;
+	char ch;
+
+    printf("데이터를 입력하세요 : ");
+    scanf("%d %d %c", &a, &b, &ch);
+    printf("입력한 값은 %d %d %c입니다\n", a, b, ch);
+	*/
+
+	
+    char name[10];
+    printf("이름을 입력하세요 : ");
+    scanf("%s", name); //1000
+    printf("내 이름은 %s입니다\n", name);//1000
+	
+}
